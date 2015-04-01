fd27fd97f738a46f63dca30076dd4f5a10f351b9
Stephane Sezer <sas@cd80.net>
2015-03-31 17:20:02 -0700

    Fix a typo in Darwin bash config.

--- bash/by-system/Darwin/aliases.sh
+++ bash/by-system/Darwin/aliases.sh
@@ -1,4 +1,4 @@
 alias ls='ls -G'
-if command -v gvim >/dev/null 2>&1 && [ -z "$SSH_CONNECTION" ]; then
+if command -v mvim >/dev/null 2>&1 && [ -z "$SSH_CONNECTION" ]; then
   alias vim='mvim --remote-tab-silent'
 fi
