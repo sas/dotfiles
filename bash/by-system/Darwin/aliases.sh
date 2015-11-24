5daf8682a3a3162b529357841dec529946dbb5b1
Stephane Sezer <sas@cd80.net>
2015-11-23 16:10:07 -0800

    Use gls if it's available on Mac.

--- bash/by-system/Darwin/aliases.sh
+++ bash/by-system/Darwin/aliases.sh
@@ -1,4 +1,9 @@
-alias ls='ls -G'
+if which -s gls; then
+  alias ls='gls --color --indicator-style=slash --group-directories-first'
+else
+  alias ls='ls -G'
+fi
+
 if command -v mvim >/dev/null 2>&1 && [ -z "$SSH_CONNECTION" ]; then
   alias vim='mvim --remote-tab-silent'
 fi
