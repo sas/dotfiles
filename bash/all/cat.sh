9bdbd16959f91a5b970daceb5f801e1e9d23c993
Stephane Sezer <sas@cd80.net>
2017-06-16 16:04:22 -0700

    Add an alias to colorize code with cat

---
+++ bash/all/cat.sh
@@ -0,0 +1,14 @@
+__is_source_file() {
+  case "$1" in
+    *.c|*.h|*.m|*.cpp|*.cc|*.mm|*.js|*.json|Makefile) return 0;;
+    *) return 1;;
+  esac
+}
+
+cat() {
+  if [ $# -eq 1 ] && [ -t 1 ] && __is_source_file "$1" && which -s vimpager; then
+    vimpager "$1"
+  else
+    "$(which cat)" "$@"
+  fi
+}
