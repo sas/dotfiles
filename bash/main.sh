1caf46cf791d4563b9d24ecaea786b8cfab3faeb
Stephane Sezer <sas@cd80.net>
2019-09-30 11:08:57 -0700

    Make bash adapt to location of dotfiles repo

--- bash/main.sh
+++ bash/main.sh
@@ -3,7 +3,7 @@ shopt -s checkwinsize
 set -o noclobber
 
 for d in utils all by-system/"$(uname -s)" by-nodename/"$(uname -n)"; do
-  for f in ~/Source/dotfiles/bash/"$d"/*.sh; do
+  for f in "$(dirname $(realpath "${BASH_SOURCE[0]}"))/$d"/*.sh; do
     source "$f"
   done
 done
